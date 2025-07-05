ab6ec551a4a6b223e734afb67cf9a346d280151c
Dipti ranjan sahoo <sahoo.diptiranjan.itm020@gmail.com>
2025-07-05 23:59:43 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,13 @@
+public class Add{
+    public static void main(String arays[])
+    {
+        int i ;
+        int  sum = 0;
+        for(i = 0 ; i <= 10; i++)
+        {
+            sum = sum +i;
+        }
+        System.out.println(sum);
+
+    }
+}
