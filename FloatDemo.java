57e8cadb11cc520800996521ee3b9f7afe76c7a0
Dipti ranjan sahoo <sahoo.diptiranjan.itm020@gmail.com>
2025-07-09 22:55:43 +0530

    This is a java program to print floating point number

---
+++ FloatDemo.java
@@ -0,0 +1,14 @@
+// This file demonstrates the creation of a class using float variables.
+public class FloatDemo {
+    public static void main(String[] args) {
+        float a = 5.5f;
+        float b = 10.2f;
+        float c = b;
+        float d = a - 2.3f;
+        System.err.println(a);
+        System.err.println(b);
+        System.err.println(c);
+        System.err.println(d);
+    }
+}
+
