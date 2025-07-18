92c4abba9d22a1861e49dc0ec3556c9ee2877020
Dipti ranjan sahoo <sahoo.diptiranjan.itm020@gmail.com>
2025-07-18 23:25:05 +0530

    Matrix addition

---
+++ matrix.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+class Matrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Matrix size input
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] a = new int[rows][cols];
+        int[][] b = new int[rows][cols];
+        int[][] sum = new int[rows][cols];
+
+        // First matrix input
+        System.out.println("Enter elements of first matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        // Second matrix input
+        System.out.println("Enter elements of second matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+
+        // Matrix addition
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        // Result output
+        System.out.println("Sum of the matrices:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
